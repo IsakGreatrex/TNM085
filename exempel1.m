4db0718c81af715e84a42a01f720070c974ef981
greatrex420 <greatrexisak10@gmail.com>
2023-01-19 17:24:46 +0100

    La till ett en simulering med Euler
    positionen ändras

---
+++ exempel1.m
@@ -0,0 +1,38 @@
+% Set the time step
+h = 0.01;
+
+% Set the final time
+tf = 10;
+
+% Set the initial time
+t = 0;
+
+% Set the mass, spring constant, and damping constant
+m1 = 1;
+m2 = 1;
+k = 10;
+c = 2;
+
+% Set the initial conditions
+x0 = [0, 1, 0, 0];
+
+% Preallocate the solution arrays
+x = zeros(4, (tf/h) + 1);
+x(:,1) = x0;
+
+% Solve the ODE using the Euler method
+i = 1;
+while t <= tf
+    x(:,i+1) = x(:,i) + h * spring_damper(t, x(:,i), m1, m2, k, c);
+    t = t + h;
+    i = i+1;
+end
+
+% Plot the solution
+time = 0:h:tf + 0.01;
+plot(time, x(1,:))
+hold on
+plot(time, x(2,:))
+xlabel('Time')
+ylabel('Position')
+legend('Mass 1', 'Mass 2')
